b1b9426cbb5e47c0ed6351ffe3d7bb56503ce1bb
andreisperid <andreisperid@gmail.com>
2024-05-10 16:57:19 -0700

    gitignore build folder

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'MIT Senseable City Lab', // Usually your GitHub org/user name.
   projectName: 'Flatburn LTE', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
