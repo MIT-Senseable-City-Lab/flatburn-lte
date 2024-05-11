e62289880d76ceffeeaedc737220738424619b07
andreisperid <andreisperid@gmail.com>
2024-05-10 18:01:31 -0700

    layout, id and font adjusts

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -4,51 +4,48 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Flatburn LTE',
-  tagline: 'Part of the City Scanner project',
-  favicon: 'img/favicon.ico',
-
-
+  title: "Flatburn LTE",
+  tagline: "Part of the City Scanner project",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://mit-senseable-city-lab.github.io/',
+  url: "https://mit-senseable-city-lab.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/flatburn-lte/',
+  baseUrl: "/flatburn-lte/",
   trailingSlash: false,
-  
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'MIT Senseable City Lab', // Usually your GitHub org/user name.
-  projectName: 'Flatburn LTE', // Usually your repo name.
+  organizationName: "MIT Senseable City Lab", // Usually your GitHub org/user name.
+  projectName: "Flatburn LTE", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: 'content',
-          sidebarPath: './sidebars.js',
+          path: "content",
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/MIT-Senseable-City-Lab/flatburn-lte/tree/main/documentation',                        
+          editUrl: "https://github.com/MIT-Senseable-City-Lab/flatburn-lte/tree/main/documentation",
         },
         // blog: {
         //   showReadingTime: true,
@@ -58,7 +55,7 @@ const config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -68,63 +65,75 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Flatburn LTE',
+        // title: "Flatburn LTE",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
-          // {
-          //   type: 'docSidebar',
-          //   sidebarId: 'tutorialSidebar',
-          //   position: 'left',
-          //   label: 'Tutorial',
-          // },
-          // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/MIT-Senseable-City-Lab/flatburn-lte/',
-            label: 'GitHub',
-            position: 'right',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "right",
+            label: "Flatburn LTE",
+          },
+          // { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/MIT-Senseable-City-Lab/flatburn-lte/",
+            label: "Project GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docum/intro',
+                label: "Get started",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Follow us",
             items: [
               {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/senseable_city_lab/',
+                label: "Website",
+                href: "https://senseable.mit.edu",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/SenseableCity',
+                label: "Instagram",
+                href: "https://www.instagram.com/senseable_city_lab/",
               },
-            ],
+              {
+                label: "Twitter",
+                href: "https://twitter.com/SenseableCity",
+              },
+            ],            
           },
           {
-            title: 'More',
+            title: " ",
             items: [
               // {
               //   label: 'Blog',
               //   to: '/blog',
               // },
               {
-                label: 'GitHub',
-                href: 'https://github.com/MIT-Senseable-City-Lab',
+                label: "GitHub",
+                href: "https://github.com/MIT-Senseable-City-Lab",
+              },
+              {
+                label: "YouTube",
+                href: "https://www.youtube.com/user/senseablecitylab",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/company/mit-senseable-city-lab/",
               },
             ],
           },
