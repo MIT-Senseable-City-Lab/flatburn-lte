26f81d7c5ecd43617ae9728eb664cc9b15f08cc8
andreisperid <andreisperid@gmail.com>
2024-12-17 16:30:39 -0300

    about as landing page

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -53,6 +53,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/MIT-Senseable-City-Lab/flatburn-lte/tree/main/documentation",
+          routeBasePath: '/about', 
         },
         // blog: {
         //   showReadingTime: true,
